31f03f8eff9adf588088b4b46d55a5bed0d9fa54
Alessio Capuano <AlessioCapuanoKira@gmail.com>
2024-12-10 22:40:41 +0100

    CIAO

---
+++ CH3REALEGIT/CH3REALEGIT/CH3REALEGITApp.swift
@@ -0,0 +1,17 @@
+//
+//  CH3REALEGITApp.swift
+//  CH3REALEGIT
+//
+//  Created by Alessio Capuano on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CH3REALEGITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CH3REALEGIT/CH3REALEGIT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CH3REALEGIT
+//
+//  Created by Alessio Capuano on 10/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
